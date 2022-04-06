3fe243da8f88f2af293c7a211a3513c5edf23b17
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-05 17:14:25 -0700

    added auth page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import Home from './components/views/Home';
+import AuthPage from './components/views/AuthPage';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { getUser } from './services/auth';
 
 function App() {
+  const user = getUser();
+  const [currentUser, setCurrentUser] = useState(user);
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className="App">
+          <Route path="/auth">
+            {!currentUser ? <AuthPage {...{ setCurrentUser }} /> : <Redirect to="/" />}
+          </Route>
+          <Route path="/">{currentUser ? <Home /> : <Redirect to="/auth" />}</Route>
+        </div>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
