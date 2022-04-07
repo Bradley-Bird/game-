c968275a531f01615c49832ee2421e8db740d3a9
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-07 10:00:14 -0700

    enemy/player position closer

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -13,7 +13,7 @@ function Home() {
   }, []);
 
   return (
-    <Container>
+    <>
       <Container>
         <div>
           <span>enemies Defeated</span>
@@ -25,48 +25,55 @@ function Home() {
           </form>
         </div>
       </Container>
-      <Container>
-        <PlayerDiv>
-          <span>HIT POINTS:</span>
-          <img src={`${process.env.PUBLIC_URL}/hero.png`} alt="" />
-        </PlayerDiv>
-        {enemies.map((enemy) => (
-          <EnemyDiv key={enemy.id}>
-            <span>{enemy.name}</span>
-            <p>HIT POINTS: {enemy.hp}</p>
-          </EnemyDiv>
-        ))}
-      </Container>
-    </Container>
+      <Grid>
+        <Container>
+          <PlayerDiv>
+            <span>HIT POINTS:</span>
+            <img src={`${process.env.PUBLIC_URL}/hero.png`} alt="" />
+          </PlayerDiv>
+        </Container>
+        <Container>
+          {enemies.map((enemy) => (
+            <EnemyDiv key={enemy.id}>
+              <span>{enemy.name}</span>
+              <p>HIT POINTS: {enemy.hp}</p>
+              <img src={`${process.env.PUBLIC_URL}/${enemy.name.toLowerCase()}.png`} alt="" />
+            </EnemyDiv>
+          ))}
+        </Container>
+      </Grid>
+    </>
   );
 }
 
 const PlayerDiv = styled.div`
-  position: absolute;
+  /* position: absolute;
   left: 0;
-  bottom: 30%;
-  height: 300px;
-  width: 300px;
-  background-color: blue;
+  bottom: 30%; */
+  /* height: 300px;
+  width: 300px; */
+  /* background-color: blue; */
+
+  margin-top: 10em;
+  display: flex;
+  padding: 10px;
   img {
     height: 200px;
-    width: 200px;
   }
   span {
-    position: absolute;
     color: white;
-    top: 0;
   }
 `;
 const EnemyDiv = styled.div`
+  display: flex;
   padding: 10px;
-  bottom: 30%;
+  margin-top: 10em;
   height: 300px;
   width: 300px;
-  background-color: blue;
+  /* background-color: blue; */
+  flex-direction: column;
   img {
-    height: 200px;
-    width: 200px;
+    height: 250px;
   }
   span {
     color: white;
@@ -79,9 +86,15 @@ const EnemyDiv = styled.div`
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   & > div {
     flex: 1;
   }
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(29rem, 1fr));
+  grid-gap: 3rem;
 `;
 export default Home;
